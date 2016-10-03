3f4633d20c6e1377a178a818cf144fc6e2f22d77
Edgar <zorro.ej@gmail.com>
2016-10-02 21:57:56 -0700

    update view helper method

--- lib/responsive_images/view_helpers.rb
+++ lib/responsive_images/view_helpers.rb
@@ -34,11 +34,11 @@ module ResponsiveImages
     def src_path image, sizes
       case device_type
       when :desktop
-        image_src = sizes[:default] == :default ? image.url : image.send(sizes[:default])
+        image_src = sizes[:default] == :default ? image.url : image.send(sizes[:default]).url
       when :tablet
-        image_src = sizes[:sizes][:tablet].present? ? image.send(sizes[:sizes][:tablet]) : image.send(sizes[:default])
+        image_src = sizes[:sizes][:tablet].present? ? image.send(sizes[:sizes][:tablet]).url : image.send(sizes[:default]).url
       when :mobile
-        image_src = sizes[:sizes][:mobile].present? ? image.send(sizes[:sizes][:mobile]) : image.send(sizes[:default])
+        image_src = sizes[:sizes][:mobile].present? ? image.send(sizes[:sizes][:mobile]).url : image.send(sizes[:default]).url
       end
     end
     
